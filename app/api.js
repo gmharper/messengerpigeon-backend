7e8e448f34e7886feeb9eef9d847838ef17c3d7c
gmharper <georgemichaelharper@gmail.com>
2025-05-20 21:52:42 +0100

    updated connection

--- app/api.js
+++ app/api.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
+const cors = require('cors');
 
 const apiRouter = require("./routers/api-router");
 
+app.use(cors());
 app.use("/api", apiRouter);
 
 app.use("/*splat", (req, res) => {
