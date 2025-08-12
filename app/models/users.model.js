13b10d05b6d892cca302f36832505dbe97f242c5
gmharper <gmharper@BOOK-I42E130TL8.localdomain>
2025-08-12 15:27:38 +0100

    page fix

--- app/models/users.model.js
+++ app/models/users.model.js
@@ -18,10 +18,10 @@ const queryAllUsers = (sort="followers_count", order="DESC", page=0, limit=20, o
   if (order && !Orders.includes(order)) {
     return Promise.reject({ status: 400, msg: "Invalid Order" });
   }
-  if (typeof page !== "number") {
+  if (typeof Number(page) != "number") {
     return Promise.reject({ status: 400, msg: "Invalid Page" });
   }
-  if (typeof limit !== "number") {
+  if (typeof Number(limit) != "number") {
     return Promise.reject({ status: 400, msg: "Invalid Page Limit" });
   }
 
